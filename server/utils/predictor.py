2edd59a9b6f9776483543ddca2b784704cedf864
Bollam Sudhakar <sudhakarbollam711948@gmail.com>
2026-04-16 22:55:32 +0530

    railway deploy

--- server/utils/predictor.py
+++ server/utils/predictor.py
@@ -1,12 +1,36 @@
-import random
+import os
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+# Get server root directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Model path
+MODEL_PATH = os.path.join(BASE_DIR, "model", "mobilenetv2_waste_classifier.h5")
+
+model = load_model(MODEL_PATH)
+
+CLASSES = ['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash']
+
+RECYCLABLE = ['cardboard', 'glass', 'metal', 'paper', 'plastic']
+
 
 def predict_image(path):
-    labels = ["plastic", "glass", "metal", "paper", "trash"]
-    label = random.choice(labels)
-    confidence = round(random.uniform(0.7, 0.99), 2)
+
+    img = load_img(path, target_size=(128, 128))
+    img_array = img_to_array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    prediction = model.predict(img_array)
+
+    class_index = np.argmax(prediction)
+    confidence = float(np.max(prediction))
+
+    label = CLASSES[class_index]
 
     return {
         "label": label,
         "confidence": confidence,
-        "recyclable": label != "trash"
+        "recyclable": label in RECYCLABLE
     }
